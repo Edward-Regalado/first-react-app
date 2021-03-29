18ed67389c6b3a545057fbb08591e9afb1c43498
Edward Regalado <tregalado30@gmail.com>
2021-03-29 16:37:54 -0700

    finished lab 02

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import data from './data.json';
 
 class App extends React.Component {
   render() {
     return (
     <div>
       <Header />
-      <Main />
+      <Main data={data} />
       <Footer />
     </div>
     );
--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import CardColumns from 'react-bootstrap/CardColumns';
 import HornedBeasts from './HornedBeasts';
-import '.Main.css'
+// import '.Main.css'
 
 import data from './data.json';
 
 class Main extends React.Component {
   render() {
     let beastArray = data.map(animal => {
-        return <HornedBeasts
-        name = {animal.keyword}
-        title = {animal.title}
-        image_url = {animal.image_url}
-        description = {animal.description}
-        />
+      return <HornedBeasts
+        name={animal.keyword}
+        title={animal.title}
+        image_url={animal.image_url}
+        description={animal.description}
+      />
     });
 
     return (
-      <div>
-        {beastArray}
-      </div>
+      <main>
+        <CardColumns>
+          {beastArray}
+        </CardColumns>
+      </main>
     );
   }
 }
